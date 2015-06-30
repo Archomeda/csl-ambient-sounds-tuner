f6fbf8463ab587375b847f47e78dfed3895f5e6e
Archomeda <archomeda@hotmail.com>
2015-06-30 15:38:42 +0200

    Fix build script to terminate on errors

--- appveyor/Build.ps1
+++ appveyor/Build.ps1
@@ -1,6 +1,8 @@
 # Restore our NuGet packages
 nuget sources Add -Name CitiesSkylines -Source $env:NUGET_CSL_URL -UserName $env:NUGET_CSL_USERNAME -Password $env:NUGET_CSL_PASSWORD -Verbosity quiet -NonInteractive
+if ($LASTEXITCODE -gt 0) { exit $LASTEXITCODE }
 nuget restore .\appveyor\packages.config -SolutionDirectory .\ -NonInteractive
+if ($LASTEXITCODE -gt 0) { exit $LASTEXITCODE }
 
 [xml]$packages = Get-Content .\appveyor\packages.config
 $referencePath = ($packages.packages.package | % {(Get-Location).ToString() + "\packages\$($_.id).$($_.version)\lib\$($_.targetFramework)\"}) -Join ";"
@@ -8,6 +10,7 @@ $referencePath = ($packages.packages.package | % {(Get-Location).ToString() + "\
 
 # Do the actual build
 msbuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /verbosity:minimal /p:ReferencePath="$referencePath"
+if ($LASTEXITCODE -gt 0) { exit $LASTEXITCODE }
 
 
 # Copy the files we need to .\bin
