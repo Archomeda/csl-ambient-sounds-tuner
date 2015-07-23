9fddf47ed300cf5522a53555bdfda7063a0662e3
Archomeda <archomeda@hotmail.com>
2015-07-23 16:09:56 +0200

    Move git access token to AppVeyor project settings

--- appveyor/PrepareNewBuildVersion.ps1
+++ appveyor/PrepareNewBuildVersion.ps1
@@ -16,7 +16,7 @@ if (!$env:APPVEYOR_PULL_REQUEST_NUMBER) {
         git config --global credential.helper store
         git config --global user.name $env:APPVEYOR_REPO_COMMIT_AUTHOR
         git config --global user.email $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
-        Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+        Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GIT_ACCESS_TOKEN):x-oauth-basic@github.com`n"
         git remote add github "https://github.com/$($env:APPVEYOR_REPO_NAME).git"
         git checkout -q $env:APPVEYOR_REPO_BRANCH
         git add "appveyor.yml"
